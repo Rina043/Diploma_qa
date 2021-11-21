f3888c72d560342862fc1d7927c8e4c4a8c5af93
Ekaterina Shipkova <katerinashipkova@gmail.com>
2021-11-21 20:11:24 +0400

    BuyGate method and tests added

---
+++ src/test/java/ru/netology/pages/CreditGate.java
@@ -0,0 +1,15 @@
+package ru.netology.pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.Card;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+
+public class CreditGate {
+
+}
